8fa592ce18afb09eed8edc70e3c374326b170248
tuannguyen_ <44093611+SledgyNguyen@users.noreply.github.com>
2021-10-08 16:27:53 +0200

    Complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { EmployeeComponent } from './employee/employee.component';
 import { ShowEmpComponent } from './employee/show-emp/show-emp.component';
 import { AddEditEmpComponent } from './employee/add-edit-emp/add-edit-emp.component';
 import { SharedService } from './shared.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,10 @@ import { SharedService } from './shared.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [SharedService],
   bootstrap: [AppComponent]
--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
+readonly APIUrl="https://localhost:5001/api";
+readonly PhotoUrl="https://localhost:5001/Photos"
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  // Deparment
+  getDepList():Observable<any[]>{
+    return this.http.get<any>(this.APIUrl + '/Department');
+  }
+
+  addDepartment(val:any){
+    return this.http.post(this.APIUrl + '/Department', val);
+  }
+
+  updateDepartment(val:any){
+    return this.http.put(this.APIUrl + '/Department', val);
+  }
+
+  deleteDepartment(val:any){
+    return this.http.delete(this.APIUrl + '/Department/' + val);
+  }
+
+  // Employee
+  getEmpList():Observable<any[]>{
+    return this.http.get<any>(this.APIUrl + '/Employee');
+  }
+
+  addEmployee(val:any){
+    return this.http.post(this.APIUrl + '/Employee', val);
+  }
+
+  updateEmployee(val:any){
+    return this.http.put(this.APIUrl + '/Employee', val);
+  }
+
+  deleteEmployee(val:any){
+    return this.http.delete(this.APIUrl + '/Employee/' + val);
+  }
+
+  UploadPhoto(val:any){
+    return this.http.post(this.APIUrl + '/Employee/SaveFile', val);
+  }
+
+  getAllDepartmentNames():Observable<any[]>{
+    return this.http.get<any[]>(this.APIUrl + '/Employee/GetAllDepartmentNames')
+  }
+
 }
